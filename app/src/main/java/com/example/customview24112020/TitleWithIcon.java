3454ec1cf6f8b78bfb1063994a65a9d8450436dc
phatdroid94 <phatdroid94@gmail.com>
2021-01-12 18:19:01 +0700

    setlayout

--- app/src/main/java/com/example/customview24112020/TitleWithIcon.java
+++ app/src/main/java/com/example/customview24112020/TitleWithIcon.java
@@ -1,7 +1,10 @@
 package com.example.customview24112020;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -9,7 +12,7 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 public class TitleWithIcon extends RelativeLayout {
-    private String mTextTitle;
+    private String mTextTitle = "";
     private int mDrawableImage;
 
     private TextView mTextView;
@@ -17,16 +20,38 @@ public class TitleWithIcon extends RelativeLayout {
 
     public TitleWithIcon(Context context) {
         super(context);
+        initView(context , null);
     }
 
     public TitleWithIcon(Context context, AttributeSet attrs) {
         super(context, attrs);
+        initView(context , attrs);
     }
 
     public TitleWithIcon(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
     public void initView(Context context , AttributeSet attributeSet){
+        if (attributeSet != null && context != null){
+            LayoutInflater layoutInflater = LayoutInflater.from(context);
+            View view = layoutInflater.inflate(R.layout.layout_title_with_icon,this , true);
+            TypedArray typedArray = context.obtainStyledAttributes(attributeSet,R.styleable.TitleWithIcon);
+            mTextTitle = typedArray.getString(R.styleable.TitleWithIcon_text_title);
+            mDrawableImage = typedArray.getResourceId(R.styleable.TitleWithIcon_drawable_icon,0);
+            typedArray.recycle();
+            mapview(view);
+        }
+    }
 
+    private void mapview(View v) {
+        mImg = v.findViewById(R.id.imageIcon);
+        mTextView = v.findViewById(R.id.textViewTitle);
+
+        if (mTextTitle.isEmpty()){
+            mTextView.setText("");
+        }else{
+            mTextView.setText(mTextTitle);
+        }
+        mImg.setImageResource(mDrawableImage);
     }
 }
